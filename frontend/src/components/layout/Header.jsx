f1f7f8bc1b829113041807911ce2055355f1488d
Nagarjuna <nagarjunajogi08@gmail.com>
2026-04-21 21:09:11 +0530

    modified header nav links

--- frontend/src/components/layout/Header.jsx
+++ frontend/src/components/layout/Header.jsx
@@ -1,50 +1,69 @@
 import { useState } from "react";
+import { FiChevronDown } from "react-icons/fi";
+import { HiOutlineMenu } from "react-icons/hi";
 import "../../styles/components/header.css";
 
 const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false);
+  const navItems = [
+  { name: "Services", dropdown: true },
+  { name: "Training", dropdown: true },
+  { name: "Solutions", dropdown: true },
+  { name: "Careers", dropdown: true },
+  { name: "About", dropdown: false },
+  { name: "Contact", dropdown: false },
+];
 
   return (
     <header className="header">
       <div className="header-container">
+
+        {/* Mobile Menu Button */}
+       <div 
+          className="menu-toggle" 
+          onClick={() => setMenuOpen(!menuOpen)}
+       >
+          <HiOutlineMenu />
+       </div>
 
         {/* Logo */}
         <div className="logo">
           INKONSUL
         </div>
 
-        {/* Desktop Nav */}
-        <nav className="nav">
-          <div className="nav-item">Services</div>
-          <div className="nav-item">Training</div>
-          <div className="nav-item">Solutions</div>
-          <div className="nav-item">Careers</div>
-          <div className="nav-item">About</div>
-          <div className="nav-item">Contact</div>
-        </nav>
+      <nav className="nav">
+        {navItems.map((item, index) => (
+          <div className="nav-item" key={index}>
+            <span className="nav-text">{item.name}</span>
+            {item.dropdown && (
+              <span className="arrow">
+                <FiChevronDown />
+              </span>
+            )}
+          </div>
+        ))}
+      </nav>
 
         <div className="auth">
           <span className="login">Login</span>
           <button className="signup-btn">Sign Up</button>
         </div>
-
-        {/* Mobile Menu Button */}
-        <div className="menu-toggle" onClick={() => setMenuOpen(!menuOpen)}>
-          ☰
-        </div>
       </div>
 
-      {/* Mobile Menu */}
-      {menuOpen && (
-        <div className="mobile-menu">
+      {/* Sidebar */}
+      <div className={`sidebar ${menuOpen ? "open" : ""}`}>
+        <div className="sidebar-content">
           <div>Services</div>
           <div>Training</div>
           <div>Solutions</div>
-          <div>About</div>
           <div>Careers</div>
+          <div>About</div>
           <div>Contact</div>
         </div>
-      )}
+      </div>
+
+      {/* Overlay */}
+      {menuOpen && <div className="overlay" onClick={() => setMenuOpen(false)}></div>}
     </header>
   );
 };
