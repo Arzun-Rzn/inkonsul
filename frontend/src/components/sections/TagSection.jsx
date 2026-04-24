92ce83dac05011ef1493a8f787f76271d7cbb1ac
Nagarjuna <nagarjunajogi08@gmail.com>
2026-04-24 09:16:14 +0530

    added tag section

---
+++ frontend/src/components/sections/TagSection.jsx
@@ -0,0 +1,21 @@
+import "../../styles/sections/tagSection.css";
+
+const TagSection = () => {
+  return (
+    <section className="tag-section">
+      <div className="tag-container">
+        <h2 className="tag-heading">
+            <span>Identify</span>
+            <span>Integrate</span>
+            <span>Implement</span>
+        </h2>
+
+        <p className="tag-subtext">
+          Transforming ideas into scalable digital solutions
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default TagSection;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,12 +2,14 @@ import Hero from "../components/sections/Hero";
 import ProductsSection from "../components/sections/ProductsSection";
 import ServicesSection from "../components/sections/ServicesSection";
 import SolutionsSection from "../components/sections/SolutionsSection";
+import TagSection from "../components/sections/TagSection";
 import TrainingSection from "../components/sections/TrainingSection";
 
 const Home = () => {
   return (
     <>
       <Hero />
+      <TagSection/>
       <SolutionsSection/>
       <ProductsSection/>
       <ServicesSection/>
