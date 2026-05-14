bc47ea9847af4f53fc7a78bd532fcf72b5c6580a
Nagarjuna <nagarjunajogi08@gmail.com>
2026-05-14 14:34:29 +0530

    updated enroll btn and modal form

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -3,7 +3,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import Layout from "../components/layout/Layout";
 import AdvanceHRProgram from "../pages/AdvanceHRProgram";
-import EnquiryPage from "../pages/EnquiryPage";
 
 const AppRoutes = () => {
   return (
@@ -12,7 +11,6 @@ const AppRoutes = () => {
         <Route element={<Layout />}>
           <Route path="/" element={<Home />} />
           <Route path="/training/advance-hr-program" element={<AdvanceHRProgram />} />
-          <Route path="/enquiry" element={<EnquiryPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
